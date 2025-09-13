f148c85ed202887479368ef8688905b0896c8a0c
Pavan Bellam <pavan94411@gmail.com>
2025-09-13 19:05:37 -0400

    fix(service-user): fix the issue with reponse when user is not found

--- app/services/user.py
+++ app/services/user.py
@@ -2,7 +2,7 @@ from app.models.user import User
 from app.db.engine import SessionLocal
 from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_500_INTERNAL_SERVER_ERROR
 from fastapi import HTTPException
-
+from sqlalchemy.exc import SQLAlchemyError
 def create_user(user: dict[str, str]) -> None:
     """Create a new user in the database."""
     db = SessionLocal()
@@ -29,10 +29,20 @@ def get_user_by_sub(sub: str) -> User | None:
         user = db.query(User).filter(User.cognito_sub == sub).first()
         
         if not user:
-            raise HTTPException(status_code=HTTP_401_UNAUTHORIZED, detail="User not found")
+            raise HTTPException(
+                status_code=HTTP_401_UNAUTHORIZED,
+                detail="User not found"
+            )
         return user
-    
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Database error"
+        )
     except Exception as e:
+        if isinstance(e, HTTPException):
+            raise
         raise HTTPException(
             status_code=HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Internal server error"
